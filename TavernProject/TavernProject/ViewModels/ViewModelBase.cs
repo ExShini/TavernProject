94a23e14f5fe247a35be5da4b9b2cd2a38aefdd2
ExShini <Shinistrike@gmail.com>
2026-04-14 14:24:17 +0300

    initial commit

---
+++ TavernProject/TavernProject/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TavernProject.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ TavernProject/TavernProject/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TavernProject.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
